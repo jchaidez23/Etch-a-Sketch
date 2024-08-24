194031bf6cf6a2acb8d5ca7400f7758e71c5c2ae
jchaidez13 <josechaidez23@gmail.com>
2024-08-24 12:55:00 -0700

    Adjusted width and height to be static

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function createSquares(){
+    try{
+        let divs = document.getElementById("container");
+        while(divs.firstChild){
+            divs.removeChild(divs.lastChild);
+        }
+    }
+    catch{
+        console.log("No squares");
+    }
     let numberSquares = getNumber();
     const container = document.querySelector("#container");
     
@@ -6,13 +15,16 @@ function createSquares(){
         var div = document.createElement("div");
         div.setAttribute("style", "color: blue; background: white;")
         div.className = "createdBoxes";
+        div.id = "createdBoxes";
         container.appendChild(div)
         for(var j = 0; j <numberSquares; j++){
             var childDiv = document.createElement("div");
             childDiv.className = "createdBoxes";
             childDiv.setAttribute("style", "color: blue; background: white; border: black 1px solid;")
             let padding = getPadding(numberSquares);
-            childDiv.style.padding = padding;
+            childDiv.style.width = padding;
+            childDiv.style.height = padding;
+
             div.appendChild(childDiv);
             
         }
@@ -20,7 +32,7 @@ function createSquares(){
 }
 
 function getPadding(squares){
-    var area = (1000 * 1000);
+    var area = 1080*1080;
     console.log(squares)
     var paddingSize = Math.floor(area/(squares*squares))/100;
     padding = String(paddingSize) + "px";
