a58273c959539a9bc3298f042d8ac1331ddbeff5
jchaidez13 <josechaidez23@gmail.com>
2024-08-24 00:37:40 -0700

    Centered table and adjusted width and height

--- script.js
+++ script.js
@@ -9,10 +9,20 @@ function createSquares(){
         for(var j = 0; j <16; j++){
             var childDiv = document.createElement("div");
             childDiv.className = "createdBoxes";
-            childDiv.setAttribute("style", "color: blue; background: white; border: black 1px solid; padding: 78px;")
+            childDiv.setAttribute("style", "color: blue; background: white; border: black 1px solid;")
+            let padding = getPadding(16);
+            childDiv.style.padding = padding;
             div.appendChild(childDiv);
             
         }
     }
 }
 
+function getPadding(squares){
+    var area = (1000 * 1000);
+    console.log(squares)
+    var paddingSize = Math.floor(area/(squares*squares))/100;
+    padding = String(paddingSize) + "px";
+    console.log(padding);
+    return padding;
+}
